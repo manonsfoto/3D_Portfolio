fe1ca423090293a21328dede5f3b1700dc9299de
Minyeong Jeong <megormin@gmail.com>
2025-01-06 19:50:28 +0100

    tried to fix type errors

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,13 +12,21 @@ import Cube from "../components/Cube";
 import Rings from "../components/Rings";
 import HeroCamera from "../components/HeroCamera";
 import Button from "../components/Button";
+import * as THREE from "three";
 
-const Hero = () => {
+const Hero: React.FC = () => {
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  // Convert array positions to Vector3
+  const targetPosition = new THREE.Vector3(...sizes.targetPosition);
+  const reactLogoPosition = new THREE.Vector3(...sizes.reactLogoPosition);
+  const cubePosition = new THREE.Vector3(...sizes.cubePosition);
+  const ringPosition = sizes.ringPosition as [number, number, number];
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -31,7 +39,6 @@ const Hero = () => {
       </div>
 
       <div className="w-full h-full absolute inset-0">
-        {/* <Leva /> */}
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
@@ -43,16 +50,17 @@ const Hero = () => {
               />
             </HeroCamera>
             <group>
-              <Target position={sizes.targetPosition} />
-              <ReactLogo position={sizes.reactLogoPosition} />
-              <Cube position={sizes.cubePosition} />
-              <Rings position={sizes.ringPosition} />
+              <Target position={targetPosition} />
+              <ReactLogo position={reactLogoPosition} />
+              <Cube position={cubePosition} />
+              <Rings position={ringPosition} />
             </group>
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
       </div>
+
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
         <a href="#contact" className="w-fit">
           <Button
